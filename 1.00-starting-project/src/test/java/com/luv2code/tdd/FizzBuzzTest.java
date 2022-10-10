1278cc48ac28d979233ff3aa1445f177feecfc4c
yoonsung <sbdyzjdla@gmail.com>
2022-10-11 05:46:41 +0900

    FizzBuzz Test
    기능구현

--- 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/tdd/FizzBuzzTest.java
@@ -2,6 +2,7 @@ package com.luv2code.tdd;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -19,6 +20,35 @@ class FizzBuzzTest {
     @DisplayName("3으로 나누어진다")
     @Test
     void testForDivisibleByThree() {
-        fail("fail");
+        String expected = "Fizz";
+
+        assertEquals(expected, FizzBuzz.compute(3), "should return Fizz");
+    }
+
+    @Order(2)
+    @DisplayName("5으로 나누어진다")
+    @Test
+    void testForDivisibleByFive() {
+        String expected = "Buzz";
+
+        assertEquals(expected, FizzBuzz.compute(5), "should return Buzz");
+    }
+
+    @Order(3)
+    @DisplayName("3과5로 나누어진다")
+    @Test
+    void testForDivisibleByThreeAndFive() {
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, FizzBuzz.compute(15), "should return FizzBuzz");
+    }
+
+    @Order(4)
+    @DisplayName("3과5로 나누어지지 않는다")
+    @Test
+    void testForNotDivisibleByThreeAndFive() {
+        String expected = "1";
+
+        assertEquals(expected, FizzBuzz.compute(1), "should return 1");
     }
 }
