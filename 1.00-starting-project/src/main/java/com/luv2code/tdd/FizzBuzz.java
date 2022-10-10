36bf5e8bcd25cc6788ad421eece5e55fa20a6231
yoonsung <sbdyzjdla@gmail.com>
2022-10-11 05:51:48 +0900

    FizzBuzz 리펙토링

--- 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ 1.00-starting-project/src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -2,6 +2,21 @@ package com.luv2code.tdd;
 
 public class FizzBuzz {
 
+    // 리펙토링
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+        if (result.isEmpty()) {
+            result.append(i);
+        }
+        return result.toString();
+    }
+    /*
     public static String compute(int i) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
             return "FizzBuzz";
@@ -15,4 +30,5 @@ public class FizzBuzz {
          return Integer.toString(i);
         }
     }
+     */
 }
