13511dc6dfeb41f9213f3ff65c582b1f62a0232a
yoonsung <sbdyzjdla@gmail.com>
2023-01-09 22:41:43 +0900

    jpa test

---
+++ 3.00-starting-project/src/test/java/com/luv2code/springmvc/StudentAndGradeServiceTest.java
@@ -0,0 +1,36 @@
+package com.luv2code.springmvc;
+
+import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestPropertySource("/application.properties")
+@SpringBootTest
+public class StudentAndGradeServiceTest {
+
+    @Autowired
+    private StudentAndGradeService studentService;
+
+    @Autowired
+    private StudentDao studentDao;
+
+    @Test
+    public void createStudentService() {
+        studentService.createStudent("Oh", "Yoonsung",
+                "sbdyzjdla@gmail.com");
+
+        CollegeStudent student = studentDao.findByEmailAddress("sbdyzjdla@gmail.com");
+
+        assertEquals("sbdyzjdla@gmail.com",
+                student.getEmailAddress(), "find by email");
+
+    }
+}
